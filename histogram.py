07740126b1c4525232522694f738378c6a25c9dd
Ethepi <livingsand7@gmail.com>
2020-10-02 21:13:46 +0300

    Наброски класса

--- histogram.py
+++ histogram.py
@@ -2,3 +2,21 @@ import matplotlib
 import scipy
 import statistics
 import pandas
+
+class Histogram:
+	"""Графическое отображение:
+	- Интервального вариационного ряда
+	- Гистограммы
+	"""
+
+	def DrawIVSTable()
+		"""Отображение таблицы интервального вариационного ряда"""
+		pass
+
+	def DrawHistogram()
+		"""Отображение гистограммы"""
+		pass
+
+	def DrawDistDensityGraph()
+		"""Отображение графика плотности распределения"""
+		pass
