35d26e9ae1822e551e876be3702e5d265d2e37e9
Ethepi <livingsand7@gmail.com>
2020-10-09 13:14:36 +0300

    Создал график плотности, добавил в импорты библиотеку seaborn, создал конструктор, изменил комментарии

--- histogram.py
+++ histogram.py
@@ -1,26 +1,43 @@
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.style.use('ggplot')
+from random import randrange
 import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 class Histogram:
 	"""Графическое отображение:
-	- Интервального вариационного ряда
+	- Графика плотности распределения
 	- Гистограммы
 	"""
 	
-	def DrawHistogram(self, table):
+	def __init__(self, table):
+		self.table = table
+
+	def DrawGraphs(self):
 		"""Вычисление количества интервалов"""
-		intervalCount = round(1 + 3.22 * math.log(len(table)))
+		intervalCount = round(1 + 3.22 * math.log(len(self.table)))
 
-		"""Параметры отображения гистограмы"""
-		n, bins, patches = plt.hist(table, intervalCount, facecolor='red', alpha=0.5, align='left')
-
-		"""Наименование графика и осей"""
-		plt.title('Гистограмма')
-		plt.xlabel('Интервалы значений')
-		plt.ylabel('Частота')
-
-		"""Отображение гистограммы"""
+		"""Создание гистограммы"""
+		plt.subplot(1,2,1)
+		n, bins, patches = plt.hist(self.table, intervalCount, facecolor='red', alpha=0.5, edgecolor='black')
+		plt.title("Гистограмма")
+		plt.ylabel("Количество значений")
+		plt.xlabel("Значения")
+		"""Создание графика плотности"""
+		plt.subplot(1,2,2)
+		sns.distplot(self.table, hist=True, kde=True, bins=intervalCount, color='darkblue', hist_kws={'edgecolor':'black'}, kde_kws={'linewidth':4})
+		plt.title("График плотности")
+		plt.ylabel("Частота")
+		plt.xlabel("Значения")
+		"""Отображение графиков"""
 		plt.show()
 		pass
+
+"""Проверка класса"""
+t = []
+for i in range(1,100):
+	t.append(randrange(-10,50))
+print(t)
+
+H = Histogram(t)
+H.DrawGraphs()
