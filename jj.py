96501cd0eb2cfdaee3cf67504561c84d06e9429a
Данила <60868519+Ethepi@users.noreply.github.com>
2020-10-13 12:25:38 +0300

    Сохранение файлов несовместимых с проектом
    Часть проекта, выполненная не классами

---
+++ jj.py
@@ -0,0 +1,17 @@
+from scipy.stats import mannwhitneyu
+from openpyxl import Workbook, load_workbook
+wb = load_workbook(str(input('Введите название файла ')))
+sheet = wb.get_sheet_by_name('Лист1')
+temp = []
+temp2 = []
+for row in sheet.rows:
+    if type(row[0].value)!=str:
+        temp.append(float(row[0].value))
+        temp2.append(float(row[1].value))
+stat, p = mannwhitneyu(temp, temp2)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+alpha = 0.05
+if p > alpha:
+    print('Распредления выборки равны (не отвергает H0)')
+else:
+    print('Распределения выборки не равны (отвергает H0)')
---
+++ statisticsV.py
@@ -0,0 +1,57 @@
+from openpyxl import load_workbook, Workbook
+import statistics
+
+
+wb = load_workbook(str(input('Введите название файла ')))   #Открытие выборки
+sheet = wb.get_sheet_by_name('Лист1')
+temp = []
+temp2 = []
+for row in sheet.rows:
+	if type(row[0].value)!=str:
+		temp.append(float(row[0].value))
+		temp2.append(float(row[1].value))	
+
+
+
+def statisticV(name):										#Расчет числовых характеристик
+	print('Размер: ', len(name))
+	print('Среднее: ', statistics.mean(name))
+	print('Дисперсия: ', statistics.variance(name))
+	print('СКО: ', statistics.stdev(name))
+	try:
+		print('Мода: ', statistics.mode(name))
+	except:
+		print('Мода: -')
+	print('Медиана: ', statistics.median(name))
+	
+def sortedV(name, name2):											#Сортировка
+	wb = Workbook()
+	ws = wb.active
+	ws.append(['Выборка 1', 'Выборка 2'])
+	name = sorted(name)
+	name2 = sorted(name2)
+	for i in range(len(name)):
+		ws.append([name[i], name2[i]])
+	sortedName = str(input('Введите название файла (куда сохранить отсортированную выборку, без ".xlsx"): '))
+	wb.save(sortedName + '.xlsx')
+
+
+re = 1
+while re<=3:
+	re = int(input("""Действие:
+	1. Вывести числовые характеристики 1 выборки
+	2. Вывести числовые характеристики 2 выборки
+	3. Отсортировать
+	4. Выход
+	Выбор: """))
+	if re==1:
+		statisticV(temp)
+	if re==2:
+		statisticV(temp2)
+	if re==3:
+		sortedV(temp, temp2)
+
+		
+		
+	
+	
