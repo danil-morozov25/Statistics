eb73ad26eb9f0a9a8b327b865634a282345119c3
danil-morozov25 <57560665+danil-morozov25@users.noreply.github.com>
2020-11-01 21:51:22 +0300

    Update mannwhitney2.py

--- mannwhitney2.py
+++ mannwhitney2.py
@@ -2,23 +2,32 @@
 #Критерий Манна-Уитни
 #Используется для сравнения двух независимых выборок (могут быть разных размеров)
 #Если какой-то элемент больше сравниваемого, то ему засчитывается 1 балл. Если элементы равны, то им засчитывается по 0,5 балла. Затем баллы элементов для каждой выборки суммируются, а меньшая полученная сумма является критерием – U-статистика. Если выборки не имеют существенных различий, то значение критерия должно быть больше критического значения для выборок соответствующего размера.
+
+
 from scipy.stats import mannwhitneyu #добавление библиотеки для расчета по формуле
 from openpyxl import Workbook, load_workbook
 
-class mannwhitneyu(): #создание класса
-    def mann(name1, name2): #добавление объектов
+class Mannwhitneyu():  #создание класса
+    def mann(name1, name2):  #добавление объектов
         stat, p = mannwhitneyu(name1, name2)
-        alpha = 0.05 #табличный критерий
-        if p > alpha: #p - расчетный критерий
-            return[stat, p, 'Распредления выборки равны (не отвергает H0)'] #вывод для пользователя
+        alpha = 0.05  #табличный критерий
+        if p > alpha:  #p - расчетный критерий
+            return[stat, p, 'Распредления выборки равны (не отвергает H0)']  #вывод для пользователя
         else:
-            return[stat, p, 'Распределения выборки не равны (отвергает H0)'] #вывод для пользователя
-#Проверка    
+            return[stat, p, 'Распределения выборки не равны (отвергает H0)']
+ #Проверка    
 wb = load_workbook(str(input('Введите название файла ')))
-sheet = wb.get_sheet_by_name('Лист1')
-name1 = [] #выборка 1 (Exel)
-name2 = [] #выборка 2 (Exel)
-for row in sheet.rows:
-    if type(row[0].value)!=str:
-        name1.append(float(row[0].value))
-        name2.append(float(row[1].value))
+sheet = wb['Манн-Уитни']
+name1 = []  #выборка 1 (Exel)
+name2 = []  #выборка 2 (Exel)
+for row in sheet.iter_rows(min_row=5, min_col=2, max_col=2):
+    for cell in row:
+        if type(cell.value)!=str:
+            name1.append(cell.value)
+
+for row in sheet.iter_rows(min_row=5, min_col=3, max_col=3, max_row = 19):
+    for cell in row:
+        if type(cell.value)!=str:
+            name2.append(cell.value)
+a = Mannwhitneyu.mann(name1, name2)
+print(a)
